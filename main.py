ee11e0217e15c2a90de029f6d06916b85cd7c203
Gurjas Singh <gurjassingh92@gmail.com>
2020-12-04 19:53:28 +0530

    added changes only in main function with some comments

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 
-from flask import Flask
+from flask import Flask # library
+## module - flask
+## class - Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # initializing object
 
-@app.route("/")
+@app.route("/") # route
 def index():
-    return "hello data science batch - soai"
+    print("print this message only in console")
+    var = 10
+    # return "hello data science batch - soai" + str(var)
+    return "hello data science batch - soai - {}".format(var)
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+if __name__ == "__main__": # starting point of the application
+    app.run(
+        host="127.0.0.1",
+        port=8000,
+        debug=True)
