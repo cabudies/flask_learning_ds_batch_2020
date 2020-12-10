bf5201763cf30cdafe4e60622c0b155049d24caf
cabudies <gurjassingh92@gmail.com>
2020-12-09 19:37:05 -0800

    added new route to delete student

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ from flask import Flask, render_template, request# library
 ## module - flask
 ## class - Flask
 
-studentsList = []
+studentsList = [
+    {
+        'name': 'Udit',
+        'college': 'DIT'
+    },
+    {
+        'name': 'Tushar',
+        'college': 'DIT'
+    },
+    {
+        'name': 'Gurjas',
+        'college': 'DIT'
+    }]
 
 app = Flask(__name__) # initializing object
 
@@ -24,6 +36,18 @@ def addStudent():
         'college': college
     })
     return "Welcome {0}. Your college is: {1}".format(user, college)
+
+@app.route("/delete/<string:name>", methods=['GET'])
+def deleteStudent(name):
+    indexToBeDeleted = -1 # 0
+    for i in range(len(studentsList)):
+        if studentsList[i]['name'] == name: # XYZ # Udit
+            indexToBeDeleted = i
+            break
+    if (indexToBeDeleted != -1):
+        del studentsList[indexToBeDeleted]
+        return "Student deleted"
+    return "Student does not exist"
 
 if __name__ == "__main__": # starting point of the application
     app.run(
